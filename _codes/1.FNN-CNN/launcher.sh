731afdb622940f3709d1d9c3181b43dc11ceb2ef
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2021-09-28 12:56:34 +0200

    Updated modules of launcher

--- _codes/1.FNN-CNN/launcher.sh
+++ _codes/1.FNN-CNN/launcher.sh
@@ -16,6 +16,6 @@
 
 #SBATCH --time=00:02:00
 
-module purge; module load ffmpeg/4.0.2 gcc/6.4.0 cuda/9.1 cudnn/7.1.3 openmpi/3.0.0 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.7 szip/2.1.1 opencv/3.4.1 python/3.6.5_ML
+module purge; module load gcc/8.3.0 ffmpeg/4.2.1 cuda/10.2 cudnn/7.6.4 nccl/2.4.8 tensorrt/6.0.1 ope    nmpi/4.0.1 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.8 szip/2.1.1 opencv/4.1.1 python/3.7.4_ML
 
 python mnist_fnn_example.py
