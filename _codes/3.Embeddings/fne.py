cd2b353b6629be5fe3319d757eeea5ce08e1f7ea
FerranPares <ferran.pares@bsc.es>
2021-11-10 12:43:15 +0100

    updating embeddings code

--- _codes/3.Embeddings/fne.py
+++ _codes/3.Embeddings/fne.py
@@ -1,11 +1,8 @@
-import os
 import numpy as np
 import tensorflow as tf
 import cv2
-from keras import applications
-import keras.backend as K
 
-def full_network_embedding(model, image_paths, batch_size, input_tensor, target_tensors, input_reshape, stats=np.empty((0, 0))):
+def full_network_embedding(model, image_paths, batch_size, target_layer_names, input_reshape, stats=None):
     ''' 
     Generates the Full-Network embedding[1] of a list of images using a pre-trained
     model (input parameter model) with its computational graph loaded. Tensors used 
@@ -28,8 +25,7 @@ def full_network_embedding(model, image_paths, batch_size, input_tensor, target_
                              using `tf.Graph.as_graph_def`.
         image_paths (list(str)): List of images to generate the FNE for.
         batch_size (int): Number of images to be concurrently computed on the same batch.
-        input_tensor (str): Name of tensor from model where the input is fed to
-        target_tensors (list(str)): List of tensor names from model to extract features from.
+        target_layer_names (list(str)): List of tensor names from model to extract features from.
         input_reshape (tuple): A tuple containing the desired shape (height, width) used to resize the image.
         stats (2D ndarray): Array of feature-wise means and stddevs for standardization.
 
@@ -37,50 +33,47 @@ def full_network_embedding(model, image_paths, batch_size, input_tensor, target_
        2D ndarray : List of features per image. Of shape <num_imgs,num_feats>
        2D ndarry: Mean and stddev per feature. Of shape <2,num_feats>
     '''
-    #Print available layers
-    #print([tensor.name for tensor in tf.get_default_graph().as_graph_def().node])
+
+    # Define feature extractor
+    feature_extractor = tf.keras.Model(
+        inputs=model.inputs,
+        outputs=[layer.output for layer in model.layers if layer.name in target_layer_names],
+    )
+    get_raw_features = lambda x: [tensor.numpy() for tensor in feature_extractor(x)]
 
     # Prepare output variable
-    len_features = 0
-    for tensor_name in target_tensors:
-        t_tensor = tf.get_default_graph().get_tensor_by_name(tensor_name)
-        len_features += t_tensor.get_shape().as_list()[-1]
+    feature_shapes = [layer.output_shape for layer in model.layers if layer.name in target_layer_names]
+    len_features = sum(shape[-1] for shape in feature_shapes)
     features = np.empty((len(image_paths), len_features))
 
-    # Prepare tensors to capture
-    x0 = tf.get_default_graph().get_tensor_by_name(input_tensor)
-    tensorOutputs = []
-    for tname in target_tensors:
-        t = tf.get_default_graph().get_tensor_by_name(tname)
-        tensorOutputs.append(t)
     # Extract features
-    with tf.Session() as sess:
-        sess.run([tf.local_variables_initializer(), tf.global_variables_initializer()])
-        for idx in range(0, len(image_paths), batch_size):
-            batch_images_path = image_paths[idx:idx+batch_size]
-            img_batch = np.zeros((len(batch_images_path), *input_reshape, 3), dtype=np.float32)
-            for i, img_path in enumerate(batch_images_path):
-                cv_img = cv2.imread(img_path)
-                try:
-                    cv_img_resize = cv2.resize(cv_img, input_reshape)
-                    img_batch[i] = np.asarray(cv_img_resize, dtype=np.float32)[:, :, ::-1]
-                except:
-                    print(img_path)
-            feature_vals = sess.run(tensorOutputs, feed_dict={x0: img_batch})
-            features_current = np.empty((len(batch_images_path), 0))
-            for feat in feature_vals:
-                #If its not a conv layer, add without pooling
-                if len(feat.shape) != 4:
-                    features_current = np.concatenate((features_current, feat), axis=1)
-                    continue
-                #If its a conv layer, do SPATIAL AVERAGE POOLING
-                pooled_vals = np.mean(np.mean(feat, axis=2), axis=1)
-                features_current = np.concatenate((features_current, pooled_vals), axis=1)
-            # Store in position
-            features[idx:idx+len(batch_images_path)] = features_current.copy()
+    for idx in range(0, len(image_paths), batch_size):
+        batch_images_path = image_paths[idx:idx + batch_size]
+        img_batch = np.zeros((len(batch_images_path), *input_reshape, 3), dtype=np.float32)
+        for i, img_path in enumerate(batch_images_path):
+            cv_img = cv2.imread(img_path)
+            try:
+                cv_img_resize = cv2.resize(cv_img, input_reshape)
+                img_batch[i] = np.asarray(cv_img_resize, dtype=np.float32)[:, :, ::-1]
+            except:
+                print(img_path)
+
+        feature_vals = get_raw_features(img_batch)
+        features_current = np.empty((len(batch_images_path), 0))
+        for feat in feature_vals:
+            #If its not a conv layer, add without pooling
+            if len(feat.shape) != 4:
+                features_current = np.concatenate((features_current, feat), axis=1)
+                continue
+            #If its a conv layer, do SPATIAL AVERAGE POOLING
+            pooled_vals = np.mean(np.mean(feat, axis=2), axis=1)
+            features_current = np.concatenate((features_current, pooled_vals), axis=1)
+        # Store in position
+        features[idx:idx+len(batch_images_path)] = features_current.copy()
+
     # STANDARDIZATION STEP
     # Compute statistics if needed
-    if len(stats) == 0:
+    if stats is None:
         stats = np.zeros((2, len_features))
         stats[0, :] = np.mean(features, axis=0)
         stats[1, :] = np.std(features, axis=0)
@@ -95,16 +88,12 @@ def full_network_embedding(model, image_paths, batch_size, input_tensor, target_
     features[features < th_neg] = -1
     features[[(features >= th_neg) & (features <= th_pos)][0]] = 0
 
-    # Store output
-    outputs_path = '../outputs'
-    if not os.path.exists(outputs_path):
-        os.makedirs(outputs_path)
-    np.save(os.path.join(outputs_path, 'fne.npy'), features)
-    np.save(os.path.join(outputs_path, 'stats.npy'), stats)
-
-    # To load output do:
-    # fne = np.load('fne.npy')
-    # fne_stats = np.load('stats.npy')
+    # # Store output
+    # outputs_path = '../outputs'
+    # if not os.path.exists(outputs_path):
+    #     os.makedirs(outputs_path)
+    # np.save(os.path.join(outputs_path, 'fne.npy'), features)
+    # np.save(os.path.join(outputs_path, 'stats.npy'), stats)
 
     # Return
     return features, stats
